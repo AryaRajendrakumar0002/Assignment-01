8597cd12c864515d2aeefd64cabab7e4d3269676
Arya <mailtoaryaraj@gmail.com>
2023-09-22 19:48:36 +0530

    Last updated

--- code4.js
+++ code4.js
@@ -1,9 +1,9 @@
 // program to find the sum of squares of array elements
 
-const array1=[1,2,3,4,5];
+const arr=[1,2,3,4,5];
 let sum=0;
-for(let i=0;i<array1.length;i++)
+for(let i=0;i<arr.length;i++)
 {
-    sum+=array1[i]*array1[i];
+    sum+=arr[i]*arr[i];
 }
 console.log("sum of squares:",sum);
